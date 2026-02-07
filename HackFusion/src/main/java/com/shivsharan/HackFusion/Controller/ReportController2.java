4c058fb6ee43a511f8f4e6ab997073b0fefa3091
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2026-02-07 16:29:48 +0530

    all Controller phase 1 done

--- HackFusion/src/main/java/com/shivsharan/HackFusion/Controller/ReportController2.java
+++ HackFusion/src/main/java/com/shivsharan/HackFusion/Controller/ReportController2.java
@@ -1,14 +1,24 @@
 package com.shivsharan.HackFusion.Controller;
 
+import com.shivsharan.HackFusion.Service.ReportService3;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.UUID;
+
 @Controller
 public class ReportController2 {
-//
-//    @PutMapping("/reopen")
-//    public ResponseEntity reOpen()
+
+    @Autowired
+    ReportService3 reportService3 ;
+
+    @PutMapping("/reopen")
+    public ResponseEntity reOpen(@RequestBody UUID reportId){
+        reportService3.reOpen(reportId);
+        return  ResponseEntity.ok().body("REOPENED");
+    }
 }
---
+++ HackFusion/src/main/java/com/shivsharan/HackFusion/Service/ReportService3.java
@@ -0,0 +1,12 @@
+package com.shivsharan.HackFusion.Service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ReportService3 {
+    public void reOpen(UUID reportId){
+
+    }
+}
