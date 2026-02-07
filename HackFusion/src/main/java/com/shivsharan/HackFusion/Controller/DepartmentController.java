2b0a2b066d80d57c85c641faf8b225cc79c46fec
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2026-02-07 15:34:24 +0530

    changes done

--- HackFusion/src/main/java/com/shivsharan/HackFusion/Controller/DepartmentController.java
+++ HackFusion/src/main/java/com/shivsharan/HackFusion/Controller/DepartmentController.java
@@ -2,9 +2,11 @@ package com.shivsharan.HackFusion.Controller;
 
 import com.shivsharan.HackFusion.DTO.assignDTO;
 import com.shivsharan.HackFusion.Service.ReportService;
+import com.shivsharan.HackFusion.Service.ReportService2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -14,6 +16,8 @@ import java.util.UUID;
 public class DepartmentController {
     @Autowired
     ReportService reportService ;
+    @Autowired
+    ReportService2 reportService2 ;
 
     @GetMapping("/getReports")
     public ResponseEntity getReports(@RequestParam UUID departmentid){
@@ -31,6 +35,18 @@ public class DepartmentController {
     public ResponseEntity<String> assignWorker(@RequestBody assignDTO dto){
         reportService.assignWorker(dto);
         return ResponseEntity.ok().body("ASSIGNMENT DONE");
+    }
+
+    @GetMapping("/getReportStatus")
+    public ResponseEntity reportStatus(@RequestParam UUID reportid){
+        reportService.getReportStatus(reportid) ;
+        return ResponseEntity.ok().body("Jell");
+    }
+
+    @PutMapping("/closeReport")
+    public ResponseEntity closeReport(@RequestBody UUID reportID){
+        reportService2.closeReport(reportID);
+        return ResponseEntity.ok().body("CLOSE THE REPORT");
     }
 
 }
---
+++ HackFusion/src/main/java/com/shivsharan/HackFusion/Service/ReportService2.java
@@ -0,0 +1,15 @@
+package com.shivsharan.HackFusion.Service;
+
+import com.shivsharan.HackFusion.Repository.ReportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ReportService2 {
+
+    public void closeReport(UUID reportId){
+
+    }
+}
